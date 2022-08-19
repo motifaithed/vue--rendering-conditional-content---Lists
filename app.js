dc0d190605ef420f4669f7c9a357a1b227df0e8a
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-19 09:43:16 +0800

    008 Removing List Items

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = Vue.createApp({
     addGoal(){
       this.goals.push(this.inputGoal);
       this.inputGoal = '';
+    },
+    removeGoal(index){
+      this.goals.splice(index,1);
     }
   },
 });
